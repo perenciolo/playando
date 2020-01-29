cacc74581d300c2e1fe2bf978ccd420aab0f4003
Gustavo Perenciolo <gustavoperenciolo@MacBook-Pro-de-Gustavo.local>
2020-01-28 22:24:16 -0300

    Updates

---
+++ precache-manifest.b7ba9075f939483d2316f9b4388e232d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56d15697d1414a3707fd75bd7b3ea727",
+    "url": "/playando/index.html"
+  },
+  {
+    "revision": "2d7bb1236fbacbea5627",
+    "url": "/playando/static/js/2.a1443601.chunk.js"
+  },
+  {
+    "revision": "ccc664cc320e64726dd95d0affdec9f0",
+    "url": "/playando/static/js/2.a1443601.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cff5dd0ac60e0442948f",
+    "url": "/playando/static/js/main.a08e0235.chunk.js"
+  },
+  {
+    "revision": "fc9318dc3547a7662c15",
+    "url": "/playando/static/js/runtime-main.36fe52ee.js"
+  }
+]);
